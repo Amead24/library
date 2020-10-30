e60674c31bc156050fb20a58f1b13dc057bc6554
Tony Mead <9004841+Amead24@users.noreply.github.com>
2020-10-30 19:54:22 -0400

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,17 @@ module.exports = {
   publicPath: '/',
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  chainWebpack(config){
+    config.module.rule('md')
+      .test(/\.md/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-markdown-loader')
+      .loader('vue-markdown-loader/lib/markdown-compiler')
+      .options({
+        raw: true
+      })
+  }
 }
